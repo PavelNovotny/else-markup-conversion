6938a634af9dfa2a434d7abe3a6dea0f778bc658
Pavel Novotny <pavel.novotny.cz@gmail.com>
2016-09-27 15:35:09 +0200

    vypocty do templaty

--- test/customer/testCustomer.js
+++ test/customer/testCustomer.js
@@ -16,8 +16,9 @@ nconf.argv()
 
 var elasticUrl = nconf.get('elastic-url') + "/customer_index_rif/customer/_search?pretty=true";
 var customerTemplate = fs.readFileSync('./templates/customerTemplate.hbs', 'utf8');
-var testData = JSON.parse(fs.readFileSync('./test/customer/proxyRequest.json', 'utf8'));
-var query = convert.searchQuery(testData, customerTemplate);
+var testProxyData = JSON.parse(fs.readFileSync('./test/customer/proxyRequest.json', 'utf8'));
+testProxyData.general.from = function() {return (testProxyData.general.page-1)*testProxyData.general.size};
+var query = convert.searchQuery(customerTemplate, testProxyData);
 fs.writeFile("test/customer/testCustomerRequest.json", query, null);
 testCommon.convertToStream(query).pipe(request.post(elasticUrl)).pipe(fs.createWriteStream("test/customer/testCustomerResult.json")) ;
 
